e8e2828353c11756a2dd6756e6506fb5f702ef89
WJX2001 <1758807220@qq.com>
2025-10-15 08:46:46 +0800

    feat: 配置CLI 应用程序

--- cmd/contracts-caller/cli.go
+++ cmd/contracts-caller/cli.go
@@ -5,7 +5,9 @@ import (
 
 	dapplink_vrf "github.com/WJX2001/contract-caller"
 	"github.com/WJX2001/contract-caller/common/cliapp"
+	"github.com/WJX2001/contract-caller/common/opio"
 	"github.com/WJX2001/contract-caller/config"
+	"github.com/WJX2001/contract-caller/database"
 	flag2 "github.com/WJX2001/contract-caller/flags"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/urfave/cli/v2"
@@ -19,9 +21,34 @@ func runDappLinkVrf(ctx *cli.Context, shutdown context.CancelCauseFunc) (cliapp.
 		log.Error("failed to load config", "err", err)
 		return nil, err
 	}
-	// return dappli
-	// return nil, nil
+	// 2. 创建 DappLinkVrf 对象
 	return dapplink_vrf.NewDappLinkVrf(ctx.Context, &cfg, shutdown)
+}
+
+// 执行数据库迁移 （Schema 升级/初始化）
+// 使用场景：首次部署或数据库结构更新时运行
+
+func runMigrations(ctx *cli.Context) error {
+	log.Info("Running migrations...")
+	cfg, err := config.LoadConfig(ctx)
+	if err != nil {
+		log.Error("failed to load config", "err", err)
+		return err
+	}
+
+	ctx.Context = opio.CancelOnInterrupt(ctx.Context)
+	db, err := database.NewDB(ctx.Context, cfg.MasterDB)
+	if err != nil {
+		log.Error("failed to connect to database", "err", err)
+		return err
+	}
+	defer func(db *database.DB) {
+		err := db.Close()
+		if err != nil {
+			return
+		}
+	}(db)
+	return db.ExecuteSQLMigration(cfg.Migrations)
 }
 
 func NewCli(GitCommit string, GitData string) *cli.App {
@@ -36,6 +63,20 @@ func NewCli(GitCommit string, GitData string) *cli.App {
 				Flags:       flags,
 				Description: "Runs the indexing service",
 				Action:      cliapp.LifecycleCmd(runDappLinkVrf),
+			},
+			{
+				Name:        "migrate",
+				Flags:       flags,
+				Description: "Runs the database migrations",
+				Action:      runMigrations,
+			},
+			{
+				Name:        "version",
+				Description: "print version",
+				Action: func(ctx *cli.Context) error {
+					cli.ShowVersion(ctx)
+					return nil
+				},
 			},
 		},
 	}
--- cmd/contracts-caller/main.go
+++ cmd/contracts-caller/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/WJX2001/contract-caller/common/opio"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+// 用于追踪生产环境运行的是哪个版本的代码
+var (
+	GitCommit = "" // Git 提交的哈希值
+	GitData   = "" // Git 提交的日期/事件
+)
+
+func main() {
+	// 创建一个终端日志处理器
+	/*
+		日志级别设置为：LevelInfo: 会记录 Info、Warn、Error 等级别的日志
+		日志输出到标准错误输出：os.Stderr¸
+		最后一个参数 true 表示启用彩色输出
+	*/
+	log.SetDefault(log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr, log.LevelInfo, true)))
+	app := NewCli(GitCommit, GitData)
+	// 创建一个带有中断阻塞器的上下文，这允许程序优雅处理系统中断信号
+	ctx := opio.WithInterruptBlocker(context.Background())
+	// 运行应用并处理错误
+	if err := app.RunContext(ctx, os.Args); err != nil {
+		log.Error("Application failed", err)
+		os.Exit(1)
+	}
+}
